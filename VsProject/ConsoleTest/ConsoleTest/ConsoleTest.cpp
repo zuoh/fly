8fb0a78de14ab1b63f861dfd302147e326f5bc42
zuoh <zuohui114@163.com>
2016-10-27 15:23:20 +0800

    Create ConsoleTest project

---
+++ VsProject/ConsoleTest/ConsoleTest/ConsoleTest.cpp
@@ -0,0 +1,15 @@
+// ConsoleTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+#include "AlgorithmTest.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AlgorithmTest test;
+	test.AutoTest();
+	return 0;
+}
+
